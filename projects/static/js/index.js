742872e21777ac6a3983db49bd933765060cd318
Mark Ignacio <en.yaye@gmail.com>
2014-02-13 01:08:19 -0500

    added employers filter and refactored projects display

--- projects/static/js/index.js
+++ projects/static/js/index.js
@@ -3,7 +3,7 @@
     $(document).ready(function(){
         var $pModal = $('#project-modal');
 
-        $('.project').on('click', function(event) {
+        $('.project > figure').on('click', function(event) {
             // don't open the modal if we're going somewhere else
             if (event.target.tagName.toLowerCase() === 'a') {
                 return;
@@ -15,5 +15,38 @@
             $pModal.append('<a class="close-reveal-modal">&#215;</a>');
             $pModal.foundation('reveal', 'open');
         });
+
+        // subnav
+        var $employerSelect = $('#employer-select');
+        var $comProjectsContainer = $('#paid-projects');
+        var $comProjects = $comProjectsContainer.find('.project');
+
+        // subnav selection events
+        $employerSelect.on('click', 'a', function (event){
+            event.preventDefault();
+        });
+        $employerSelect.on('click', 'dd', function (event){
+            // todo: more flexible implementation. maybe data-?
+            var classList = event.currentTarget.classList;
+            if (classList.length > 0) {
+                var slug = event.currentTarget.classList[0];
+                // show everything
+                if (slug === 'all') {
+                    $comProjects.show();
+                }
+
+                // only show matching slug
+                else {
+                    $comProjects.hide().promise().done(function() {
+                        $comProjectsContainer.find('.' + slug).fadeIn('fast');
+                    });
+                }
+            }
+
+            // visual cue
+            $employerSelect.find('.active').removeClass('active');
+            $(event.currentTarget).addClass('active');
+        });
+
     });
 })();
--- projects/views.py
+++ projects/views.py
@@ -2,12 +2,13 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 from mini_cms.models import Taxonomy
-from models import PersonalProject, CommissionedProject
+from models import PersonalProject, CommissionedProject, Employer
 
 
 def index(request):
     personal_projects = PersonalProject.objects.all()
-    paid_projects = CommissionedProject.objects.all()
+    paid_projects = CommissionedProject.objects.select_related('employer').all()
+    employers = Employer.objects.filter(commissionedproject__in=paid_projects).distinct()
 
     # just going to go ahead and require these
     whoami = Taxonomy.objects.get(key='whoami').get_val()
@@ -16,6 +17,7 @@ def index(request):
     data = {
         'personal_projects': personal_projects,
         'paid_projects': paid_projects,
+        'employers': employers,
         'whoami': whoami,
         'projects_blurb': projects_blurb
     }
