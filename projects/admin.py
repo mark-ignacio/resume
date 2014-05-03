f6598cb5df141e936301fd3b1d7e2411c34d2a5f
Mark Ignacio <mark@ignacio.io>
2014-05-02 22:33:27 -0400

    completed tag model layer and admin interface

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
+from django.contrib.contenttypes import generic
 
-from models import CommissionedProject, PersonalProject, Employer, Client
+from projects.models import CommissionedProject, PersonalProject, Employer, Client, TagCategory, Tag
+
+
+class TagInlineAdmin(generic.GenericTabularInline):
+    model = Tag
+    fields = ('name', 'slug', 'category')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class TaggedProjectAdmin(admin.ModelAdmin):
+    inlines = (TagInlineAdmin,)
+
+
+class TagCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_filter = ('category',)
+
 
 admin.site.register(Client)
 admin.site.register(Employer)
-admin.site.register(CommissionedProject)
-admin.site.register(PersonalProject)
+admin.site.register(CommissionedProject, TaggedProjectAdmin)
+admin.site.register(PersonalProject, TaggedProjectAdmin)
+admin.site.register(TagCategory, TagCategoryAdmin)
+admin.site.register(Tag, TagAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 
@@ -27,6 +29,8 @@ class Project(models.Model):
     start_date = models.DateField(blank=True, null=True)
     end_date = models.DateField(blank=True, null=True, help_text='Leave blank to display "Current"')
 
+    tags = generic.GenericRelation('Tag')
+
     class Meta:
         abstract = True
         ordering = ('-start_date',)
@@ -42,3 +46,26 @@ class PersonalProject(Project):
 class CommissionedProject(Project):
     employer = models.ForeignKey(Employer, blank=True)
     clients = models.ManyToManyField(Client, blank=True)
+
+
+class TagCategory(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Tag categories'
+
+    def __unicode__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.SlugField()
+    category = models.ForeignKey(TagCategory, related_name='tags')
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+
+    def __unicode__(self):
+        return self.name
