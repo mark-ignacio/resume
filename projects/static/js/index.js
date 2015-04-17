4638b805db901dd77518ff4901bb04a8b56fae09
Mark Ignacio <mark@ignacio.io>
2015-04-17 00:51:37 -0400

    remove a bunch from the schema and make a new front page

--- projects/static/js/index.js
+++ projects/static/js/index.js
@@ -1,57 +1,18 @@
 "use strict";
-(function() {
-    $(document).ready(function(){
-        var $pModal = $('#project-modal');
+(function () {
+    var $window = $(window);
+    var $lpage = $('.lpage');
 
-        $('.project > figure').on('click', function(event) {
-            // don't open the modal if we're going somewhere else
-            if (event.target.tagName.toLowerCase() === 'a') {
-                return;
-            }
+    var pages = [
+        [null, 'index', null],
+        ['dev', 'security', 'hobby'],
+        [null, 'about', null]
+    ];
+    var currentPage = [0, 1];
+    // bind arrow keys
 
-           // fill the modal
-            event.preventDefault();
-            $pModal.html(this.innerHTML);
-            $pModal.append('<a class="close-reveal-modal">&#215;</a>');
-            $pModal.foundation('reveal', 'open');
-        });
+    $('nav > .up').click(function (e) {
+        e.preventDefault();
 
-        // subnav
-        var $employerSelect = $('#employer-select');
-        var $comProjectsContainer = $('#paid-projects');
-        var $comProjects = $comProjectsContainer.find('.project');
-
-        // subnav selection events
-        $employerSelect.on('click', 'a', function (event){
-            event.preventDefault();
-        });
-        $employerSelect.on('click', 'dd', function (event){
-            // todo: more flexible implementation. maybe data-?
-            var classList = event.currentTarget.classList;
-
-            // ignore clicks on active selection
-            if (event.currentTarget.className.indexOf('active') !== -1) {
-                return;
-            }
-            if (classList.length > 0) {
-                var slug = event.currentTarget.classList[0];
-                // show everything
-                if (slug === 'all') {
-                    $comProjects.fadeIn('fast');
-                }
-
-                // only show matching slug
-                else {
-                    $comProjects.hide().promise().done(function() {
-                        $comProjectsContainer.find('.' + slug).fadeIn('fast');
-                    });
-                }
-            }
-
-            // visual cue
-            $employerSelect.find('.active').removeClass('active');
-            $(event.currentTarget).addClass('active');
-        });
-
-    });
+    })
 })();
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,6 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-from mini_cms.models import Taxonomy
 from models import PersonalProject, CommissionedProject, Employer
 
 
@@ -10,15 +9,9 @@ def index(request):
     paid_projects = CommissionedProject.objects.select_related('employer').all()
     employers = Employer.objects.filter(commissionedproject__in=paid_projects).distinct()
 
-    # just going to go ahead and require these
-    whoami = Taxonomy.objects.get(key='whoami').get_val()
-    projects_blurb = Taxonomy.objects.get(key='projects_blurb').get_val()
-
     data = {
         'personal_projects': personal_projects,
         'paid_projects': paid_projects,
         'employers': employers,
-        'whoami': whoami,
-        'projects_blurb': projects_blurb
     }
     return render_to_response('index.html', data, RequestContext(request))
