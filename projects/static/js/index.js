09c4dde2379d9f2f9d7996348748bb0b048479e7
Mark Ignacio <mark@ignacio.io>
2015-04-18 17:10:47 -0400

    new design!

--- projects/static/js/index.js
+++ projects/static/js/index.js
@@ -1,7 +1,11 @@
 "use strict";
+
 (function () {
+
+    // cache a bunch of selectors
     var $window = $(window);
-    var $lpage = $('.lpage');
+    var $index = $('#index');
+    var $lpages = $('#other-lpages');
 
     var pages = [
         [null, 'index', null],
@@ -9,10 +13,61 @@
         [null, 'about', null]
     ];
     var currentPage = [0, 1];
-    // bind arrow keys
 
-    $('nav > .up').click(function (e) {
-        e.preventDefault();
+    function loadPage(page, direction) {
+        var $page = $(page);
+        var $oldPage = $('#' + pages[currentPage[0]][currentPage[1]]);
+        // animate current page going up
 
-    })
+        switch (direction) {
+            case 'up':
+                $page.css({
+                    top: $window.height() * 2,
+                    left: 0,
+                    right: 0,
+                    bottom: $window.height()
+                });
+                break;
+            case 'down':
+                $page.css({
+                    top: $window.height(),
+                    left: 0,
+                    right: 0,
+                    bottom: $window.height() * 2
+                });
+                break;
+            default:
+                break;
+        }
+
+        $oldPage.animate(
+            {top: $window.height() * -1},
+            function () {
+                $oldPage.css({top: -9999})
+            }
+        );
+        $page.animate({
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0
+        });
+    }
+
+    // vim keybinds are ok
+    $(document.body).keyup(function (e) {
+        if ((e.which === 40 || e.which === 74)) {
+            loadPage('#security', 'down');
+        }
+    });
+
+    // on startup and scroll, resize
+    var sectionResize = function () {
+        var windowHeight = $window.height();
+        $index.height(windowHeight);
+        $lpages.css({top: windowHeight});
+    };
+
+    $window.resize(sectionResize);
+    sectionResize();
 })();
